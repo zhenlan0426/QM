1f8143aa698640f173ccc142ab3dd67e4678d5de
will <zhenlanwang0426@gmail.com>
2019-06-30 10:37:24 -0400

    Add files via upload

--- functions.py
+++ functions.py
@@ -8,9 +8,11 @@ Created on Sun Jun  9 13:57:40 2019
 import torch
 import torch.nn.functional as F
 from torch.nn import Sequential, Linear, ReLU, GRU,BatchNorm1d,Dropout,RReLU
-from torch_geometric.nn import NNConv,GATConv,Set2Set
+from torch_geometric.nn import NNConv,GATConv
 import torch.nn as nn
 import numpy as np
+from torch_scatter import scatter_add
+from torch_geometric.utils import softmax
 
 def Rx():
     theta = torch.rand(1)*np.pi*2
@@ -36,6 +38,51 @@ def transform_xyz(node):
     x[:,:3] = torch.matmul(R(),x[:,:3].t()).t()
     return node
 
+
+
+class Set2Set(torch.nn.Module):
+    r"""modify the oringinal Set2Set by allowing to pass in h and q_star
+    """
+
+    def __init__(self, in_channels, processing_steps, num_layers=1):
+        super(Set2Set, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = 2 * in_channels
+        self.processing_steps = processing_steps
+        self.num_layers = num_layers
+
+        self.lstm = torch.nn.LSTM(self.out_channels, self.in_channels,
+                                  num_layers)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lstm.reset_parameters()
+
+    def forward(self, x, batch, h=None,q_star=None):
+        """"""
+        batch_size = batch.max().item() + 1
+        if h is None:
+            h = (x.new_zeros((self.num_layers, batch_size, self.in_channels)),
+                 x.new_zeros((self.num_layers, batch_size, self.in_channels)))
+        if q_star is None:
+            q_star = x.new_zeros(batch_size, self.out_channels)
+
+        for i in range(self.processing_steps):
+            q, h = self.lstm(q_star.unsqueeze(0), h)
+            q = q.view(batch_size, self.in_channels)
+            e = (x * q[batch]).sum(dim=-1, keepdim=True)
+            a = softmax(e, batch, num_nodes=batch_size)
+            r = scatter_add(a * x, batch, dim=0, dim_size=batch_size)
+            q_star = torch.cat([q, r], dim=-1)
+
+        return q_star
+
+    def __repr__(self):
+        return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
+                                   self.out_channels)
+        
 class InteractionNet(torch.nn.Module):
     def __init__(self,IntDim,xDimList,FunList):
         # None in FunList mean identity func
@@ -539,3 +586,233 @@ class Net_int_2Edges_pool_intHead2(torch.nn.Module):
             return loss
         else:
             return yhat
+
+class Net_int_2Edges_Set2Set(torch.nn.Module):
+    # use set2set as head
+    def __init__(self,dim=64,edge_dim=12,node_in=8,edge_in=19,edge_in3=8):
+        super(Net_int_2Edges_Set2Set, self).__init__()
+        self.lin_node = torch.nn.Linear(node_in, dim)
+        self.lin_edge_attr = torch.nn.Linear(edge_in, edge_dim)
+        
+        nn1 = Linear(edge_dim, dim * dim, bias=False)
+        nn2 = Linear(edge_in3, dim * dim * 2 * 2, bias=False)
+        
+        self.conv1 = NNConv(dim, dim, nn1, aggr='mean', root_weight=False)
+        self.gru1 = GRU(dim, dim)
+        self.lin_covert = Sequential(BatchNorm1d(dim),Linear(dim, dim*2), \
+                                     RReLU(), Dropout(),Linear(dim*2, dim*2),RReLU())
+        
+        self.conv2 = NNConv(dim*2, dim*2, nn2, aggr='mean', root_weight=False)
+        self.gru2 = GRU(dim*2, dim*2)
+        
+        self.lin_weight = Linear(8, dim*2*2, bias=False)
+        self.lin_bias = Linear(8, 1, bias=False)
+        
+        self.norm = BatchNorm1d(dim*2*2)
+        self.norm_x = BatchNorm1d(node_in)
+        self.set2set = Set2Set(dim*2,processing_steps=3,num_layers=3)
+        
+    def forward(self, data,IsTrain=False):
+        out = F.rrelu(self.lin_node(self.norm_x(data.x)))
+        edge_attr = F.rrelu(self.lin_edge_attr(data.edge_attr))
+        h = out.unsqueeze(0)
+        # edge_*3 only does not repeat for undirected graph. Hence need to add (j,i) to (i,j) in edges
+        edge_index3 = torch.cat([data.edge_index3,data.edge_index3[[1,0]]],1)
+        edge_attr3 = torch.cat([data.edge_attr3,data.edge_attr3],0)
+        
+        for i in range(2):
+            # using bonding as edge
+            m = F.rrelu(self.conv1(out, data.edge_index, edge_attr))
+            out, h = self.gru1(m.unsqueeze(0), h)
+            out = out.squeeze(0)
+        
+        out = self.lin_covert(out)
+        h = out.unsqueeze(0)
+        for i in range(2):
+            # using couping as edge
+            m = F.rrelu(self.conv2(out, edge_index3, edge_attr3))
+            out, h = self.gru2(m.unsqueeze(0), h)
+            out = out.squeeze(0)  
+            
+        n = data.edge_index3.shape[1]
+        range_ = torch.arange(n)
+        batch_index = torch.cat([range_,range_]).to('cuda:0')
+        temp = out[data.edge_index3].reshape(2*n,-1) # (2*n_target,d)
+        yhat = self.set2set(temp,batch_index)  
+        yhat = self.norm(yhat)
+        
+        weight = self.lin_weight(data.edge_attr3)
+        bias = self.lin_bias(data.edge_attr3)
+        yhat = torch.sum(yhat * weight,1,keepdim=True) + bias
+        yhat = yhat.squeeze(1)
+        
+        if IsTrain:
+            k = torch.sum(data.edge_attr3,0)
+            nonzeroIndex = torch.nonzero(k).squeeze(1)
+            abs_ = torch.abs(data.y-yhat).unsqueeze(1)
+            loss = torch.sum(torch.log(torch.sum(abs_ * data.edge_attr3[:,nonzeroIndex],0)/k[nonzeroIndex]))/nonzeroIndex.shape[0]
+            return loss
+        else:
+            return yhat
+        
+class Net_int_2Edges_Set2Set2(torch.nn.Module):
+    # use set2set as head
+    def __init__(self,dim=64,edge_dim=12,node_in=8,edge_in=19,edge_in3=8,num_layers=1):
+        super(Net_int_2Edges_Set2Set2, self).__init__()
+        self.num_layers = num_layers
+        self.dim = dim
+        self.lin_node = torch.nn.Linear(node_in, dim)
+        self.lin_edge_attr = torch.nn.Linear(edge_in, edge_dim)
+        
+        nn1 = Linear(edge_dim, dim * dim, bias=False)
+        nn2 = Linear(edge_in3, dim * dim * 2 * 2, bias=False)
+        
+        self.conv1 = NNConv(dim, dim, nn1, aggr='mean', root_weight=False)
+        self.gru1 = GRU(dim, dim)
+        self.lin_covert = Sequential(BatchNorm1d(dim),Linear(dim, dim*2), \
+                                     RReLU(), Dropout(),Linear(dim*2, dim*2),RReLU())
+        
+        self.conv2 = NNConv(dim*2, dim*2, nn2, aggr='mean', root_weight=False)
+        self.gru2 = GRU(dim*2, dim*2)
+        
+        self.lin_weight = Linear(edge_in3, dim*2*2, bias=False)
+        self.lin_bias = Linear(edge_in3, 1, bias=False)
+        
+        self.norm = BatchNorm1d(dim*2*2)
+        self.norm_x = BatchNorm1d(node_in)
+        self.head = Set2Set(dim*2,processing_steps=3,num_layers=num_layers)
+        self.h_lin = Linear(edge_in3,num_layers*dim*2*2)
+        self.q_star_lin = Linear(edge_in3,dim*2*2)
+        
+    def forward(self, data,IsTrain=False):
+        out = F.rrelu(self.lin_node(self.norm_x(data.x)))
+        edge_attr = F.rrelu(self.lin_edge_attr(data.edge_attr))
+        h = out.unsqueeze(0)
+        # edge_*3 only does not repeat for undirected graph. Hence need to add (j,i) to (i,j) in edges
+        edge_index3 = torch.cat([data.edge_index3,data.edge_index3[[1,0]]],1)
+        edge_attr3 = torch.cat([data.edge_attr3,data.edge_attr3],0)
+        
+        for i in range(2):
+            # using bonding as edge
+            m = F.rrelu(self.conv1(out, data.edge_index, edge_attr))
+            out, h = self.gru1(m.unsqueeze(0), h)
+            out = out.squeeze(0)
+        
+        out = self.lin_covert(out)
+        h = out.unsqueeze(0)
+        for i in range(2):
+            # using couping as edge
+            m = F.rrelu(self.conv2(out, edge_index3, edge_attr3))
+            out, h = self.gru2(m.unsqueeze(0), h)
+            out = out.squeeze(0)  
+            
+        n = data.edge_index3.shape[1]
+        range_ = torch.arange(n)
+        batch_index = torch.cat([range_,range_]).to('cuda:0')
+        temp = out[data.edge_index3].reshape(2*n,-1) # (2*n_target,d)
+        
+        h0,h1 = self.h_lin(data.edge_attr3).split(self.num_layers*self.dim*2,1)
+        h = (h0.reshape(-1,self.num_layers,self.dim*2).transpose(0,1).contiguous(),\
+             h1.reshape(-1,self.num_layers,self.dim*2).transpose(0,1).contiguous())
+        q_star = self.q_star_lin(data.edge_attr3)
+            
+        yhat = self.head(temp,batch_index,h,q_star)  
+        yhat = self.norm(yhat)
+        
+        weight = self.lin_weight(data.edge_attr3)
+        bias = self.lin_bias(data.edge_attr3)
+        yhat = torch.sum(yhat * weight,1,keepdim=True) + bias
+        yhat = yhat.squeeze(1)
+        
+        if IsTrain:
+            k = torch.sum(data.edge_attr3,0)
+            nonzeroIndex = torch.nonzero(k).squeeze(1)
+            abs_ = torch.abs(data.y-yhat).unsqueeze(1)
+            loss = torch.sum(torch.log(torch.sum(abs_ * data.edge_attr3[:,nonzeroIndex],0)/k[nonzeroIndex]))/nonzeroIndex.shape[0]
+            return loss
+        else:
+            return yhat
+        
+class Net_int_2Edges_Set2Set3(torch.nn.Module):
+    # use set2set as head, pool->q_star, type->h0
+    def __init__(self,dim=64,edge_dim=12,node_in=8,edge_in=19,edge_in3=8,num_layers=1):
+        super(Net_int_2Edges_Set2Set3, self).__init__()
+        self.num_layers = num_layers
+        self.dim = dim
+        self.lin_node = torch.nn.Linear(node_in, dim)
+        self.lin_edge_attr = torch.nn.Linear(edge_in, edge_dim)
+        
+        nn1 = Linear(edge_dim, dim * dim, bias=False)
+        nn2 = Linear(edge_in3, dim * dim * 2 * 2, bias=False)
+        
+        self.conv1 = NNConv(dim, dim, nn1, aggr='mean', root_weight=False)
+        self.gru1 = GRU(dim, dim)
+        self.lin_covert = Sequential(BatchNorm1d(dim),Linear(dim, dim*2), \
+                                     RReLU(), Dropout(),Linear(dim*2, dim*2),RReLU())
+        
+        self.conv2 = NNConv(dim*2, dim*2, nn2, aggr='mean', root_weight=False)
+        self.gru2 = GRU(dim*2, dim*2)
+        
+        self.lin_weight = Linear(edge_in3, dim*2*2, bias=False)
+        self.lin_bias = Linear(edge_in3, 1, bias=False)
+        
+        self.norm = BatchNorm1d(dim*2*2)
+        self.norm_x = BatchNorm1d(node_in)
+        self.head = Set2Set(dim*2,processing_steps=3,num_layers=num_layers)
+        self.pool = Set2Set(dim*2,processing_steps=3)
+        
+        self.h_lin = Linear(edge_in3,num_layers*dim*2*2)
+        self.q_star_lin = Linear(dim*2*2,dim*2*2)
+        
+    def forward(self, data,IsTrain=False):
+        out = F.rrelu(self.lin_node(self.norm_x(data.x)))
+        edge_attr = F.rrelu(self.lin_edge_attr(data.edge_attr))
+        h = out.unsqueeze(0)
+        # edge_*3 only does not repeat for undirected graph. Hence need to add (j,i) to (i,j) in edges
+        edge_index3 = torch.cat([data.edge_index3,data.edge_index3[[1,0]]],1)
+        edge_attr3 = torch.cat([data.edge_attr3,data.edge_attr3],0)
+        
+        for i in range(2):
+            # using bonding as edge
+            m = F.rrelu(self.conv1(out, data.edge_index, edge_attr))
+            out, h = self.gru1(m.unsqueeze(0), h)
+            out = out.squeeze(0)
+        
+        out = self.lin_covert(out)
+        h = out.unsqueeze(0)
+        for i in range(2):
+            # using couping as edge
+            m = F.rrelu(self.conv2(out, edge_index3, edge_attr3))
+            out, h = self.gru2(m.unsqueeze(0), h)
+            out = out.squeeze(0)  
+            
+        n = data.edge_index3.shape[1]
+        range_ = torch.arange(n)
+        batch_index = torch.cat([range_,range_]).to('cuda:0')
+        temp = out[data.edge_index3].reshape(2*n,-1) # (2*n_target,d)
+
+        coupling_batch_index = data.batch[data.edge_index3[0]]
+        pool = self.pool(out, data.batch) # (m,d)
+        pool = pool[coupling_batch_index] # (n_target,d)        
+        
+        h0,h1 = self.h_lin(data.edge_attr3).split(self.num_layers*self.dim*2,1)
+        h = (h0.reshape(-1,self.num_layers,self.dim*2).transpose(0,1).contiguous(),\
+             h1.reshape(-1,self.num_layers,self.dim*2).transpose(0,1).contiguous())
+        q_star = self.q_star_lin(pool)
+            
+        yhat = self.head(temp,batch_index,h,q_star)  
+        yhat = self.norm(yhat)
+        
+        weight = self.lin_weight(data.edge_attr3)
+        bias = self.lin_bias(data.edge_attr3)
+        yhat = torch.sum(yhat * weight,1,keepdim=True) + bias
+        yhat = yhat.squeeze(1)
+        
+        if IsTrain:
+            k = torch.sum(data.edge_attr3,0)
+            nonzeroIndex = torch.nonzero(k).squeeze(1)
+            abs_ = torch.abs(data.y-yhat).unsqueeze(1)
+            loss = torch.sum(torch.log(torch.sum(abs_ * data.edge_attr3[:,nonzeroIndex],0)/k[nonzeroIndex]))/nonzeroIndex.shape[0]
+            return loss
+        else:
+            return yhat       
